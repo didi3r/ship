06ac90eb08fd2674f6c04ca50915d0c875d21d22
didi3r <mail@didierperez.com>
2015-05-28 18:26:50 -0500

    progress made in office

--- js/MoringaApp.js
+++ js/MoringaApp.js
@@ -8,6 +8,13 @@ app.directive('shipment', function(){
 	};
 });
 
+app.directive('sale', function(){
+	return {
+		restrict: 'E',
+		templateUrl: 'views/sale-info.html'
+	};
+});
+
 app.directive('collapsablePanel', function(){
 	return {
 		restrict: 'A',
@@ -46,69 +53,13 @@ app.controller('MenuCtrl', ['$scope', function ($scope) {
     });
 }]);
 
-app.controller('ShipmentsCtrl', ['$scope', function ($scope) {
-	$scope.shipments = [
-		{
-			id: 1,
-			delivery: {
-				addressee: 'Jhon Doe',
-				address: {
-					street: 'Azul Marino #124',
-					zone: 'Colores',
-					zip: '58000',
-					city: 'Monterrey, Nuevo León'
-				},
-				phone: '443 312 4578'
-			},
-			package: {
-				content: ['500gr Hoja Seca', '1kg Polvo']
-			},
-			courier: 'Estafeta',
-			status: 'Pendiente',
-			date: '22/05/2015'
-		},
-		{
-			id: 2,
-			delivery: {
-				addressee: 'José Camargo',
-				address: {
-					street: 'Tequila #21',
-					zone: 'Jornaleros',
-					zip: '58150',
-					city: 'Guadalajara, Jalisco'
-				},
-				phone: '443 326 3256'
-			},
-			package: {
-				content: ['3kg Polvo']
-			},
-			courier: 'Estafeta',
-			status: 'Enviado',
-			date: '17/05/2015',
-			comments: 'Lorem ipsum dolor sit amey consequteur amit...'
-		},
-		{
-			id: 3,
-			delivery: {
-				addressee: 'Martha Flores',
-				address: {
-					street: 'Batman #4',
-					zone: 'Super Heroes',
-					zip: '85421',
-					city: 'Merida, Yucatán'
-				},
-				phone: '443 985 6251'
-			},
-			package: {
-				content: ['2 Bolsas 100gr Hoja', '100gr Semilla', '1 Jabón']
-			},
-			courier: 'Estafeta',
-			status: 'Cancelado',
-			date: '20/05/2015'
-		}
-	];
-
+app.controller('ShipmentsCtrl', ['$scope', '$http', function ($scope, $http) {
 	$scope.orderBy = '-date';
+	$scope.shipments = [];
+    
+    $http.get('data/data.json').success(function(data) {
+        $scope.shipments = data.items;
+    });
 
 }]);
 
@@ -134,56 +85,11 @@ app.controller('AddSaleCtrl', ['$scope', function ($scope) {
 
 }]);
 
-app.controller('SalesListCtrl', ['$scope', function ($scope) {
-	$scope.sales = [
-		{
-			id: 1,
-			date: new Date(),
-			customer: 'Jhon Doe',
-			items: ['500gr Polvo', '2kg Hoja Seca'],
-			MLUser: 'RUGE8906',
-			email: 'ruge89_06@hotmail.com',
-			delivery: {
-				addressee: 'José Camargo',
-				address: {
-					street: 'Tequila #21',
-					zone: 'Jornaleros',
-					zip: '58150',
-					city: 'Guadalajara, Jalisco'
-				},
-				phone: '443 326 3256'
-			},
-			courier: 'Estafeta',
-			paymentStatus: 'Pagado',
-			grandTotal: 850,
-			shippingCost: 100,
-			commission: 7.50,
-			productionCost: 60,
-			totalProfit: 400
-		},
-		{
-			id: 2,
-			date: new Date(),
-			customer: 'José Perez León',
-			items: ['1kg Polvo', '100gr Semilla'],
-			MLUser: 'RUIZ066',
-			email: 'RUIZ.066@hotmail.com',delivery: {
-				addressee: 'Martha Flores',
-				address: {
-					street: 'Batman #4',
-					zone: 'Super Heroes',
-					zip: '85421',
-					city: 'Merida, Yucatán'
-				},
-				phone: '443 985 6251'
-			},
-			courier: 'Estafeta',
-			paymentStatus: 'Pendiente',
-			grandTotal: 850,
-			shippingCost: 100,
-			commission: 7.50,
-			productionCost: 60,
-			totalProfit: 400
-		}
-	];
+app.controller('SalesListCtrl', ['$scope', '$http', function ($scope, $http) {
+	$scope.sales = [];
+    
+    $http.get('data/data.json').success(function(data) {
+        $scope.sales = data.items;
+    });
+    
 }]);
