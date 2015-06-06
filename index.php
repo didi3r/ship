6fc360df402c451672a8ef56c7b200f7ab19ff18
didi3r <mail@didierperez.com>
2015-06-05 20:10:05 -0500

    CI configurations per environment

--- index.php
+++ index.php
@@ -53,7 +53,14 @@
  *
  * NOTE: If you change these, also change the error_reporting() code below
  */
-	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
+	// define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
+
+if ($_SERVER['SERVER_NAME'] == 'bioleafy.com') {
+    define('ENVIRONMENT', 'production');
+} else {
+    define('ENVIRONMENT', 'development');
+}
+
 
 /*
  *---------------------------------------------------------------
