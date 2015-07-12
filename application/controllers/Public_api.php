be1d0f005932e2b94e17c1391a05846eb96a8bfd
Didier Perez <mail@didierperez.com>
2015-07-12 16:45:25 -0500

    webhook v1 working

--- application/controllers/Public_api.php
+++ application/controllers/Public_api.php
@@ -12,50 +12,57 @@ class Public_api extends CI_Controller {
 		$post = file_get_contents("php://input");
 
 		if($post) {
-			$params = json_decode($post);
+			$post = html_entity_decode(preg_replace("/u([0-9A-F]{4})/i", "&#x\\1;", $post), ENT_NOQUOTES, 'UTF-8');
+			$params = (array) json_decode($post);
 
-			$params['total_shipping'] = (float) $params['total_shipping'];
-			$params['subtotal'] = (float) $params['subtotal'];
-			$params['total_discount'] = (float) $params['total_shipping'];
+			$order = (array) $params['order'];
+
+			$billing_address = $order['billing_address'];
+			if(!is_array($billing_address)) $billing_address = (array) $billing_address;
+
+			$order['total_shipping'] = (float) $order['total_shipping'];
+			$order['subtotal'] = (float) $order['subtotal'];
+			$order['total_discount'] = (float) $order['total_shipping'];
 
 			$package = array();
-			foreach ($params['line_items'] as $product) {
+			foreach ($order['line_items'] as $product) {
 				if(!is_array($product)) $product = (array) $product;
 				$package[] = $product['quantity'] . ' ' . $product['name'];
 			}
 
-			$shiping_address = $params['shipping_address'];
+			$shipping_address = $order['shipping_address'];
 			if(!is_array($shipping_address)) $shipping_address = (array) $shipping_address;
-			$address = $shipping_address['address_1'] . '\n';
-			$address .= $shipping_address['address_2'] . '\n';
-			$address .= $shipping_address['city'] . ', ' . $shipping_address['state'] . '\n';
-			$address .= $shipping_address['postcode'] . '\n';
+			$address = $shipping_address['address_1'] . "\r\n";
+			$address .= $shipping_address['address_2'] . "\r\n";
+			$address .= $shipping_address['city'] . ', ' . $shipping_address['state'] . "\r\n";
+			$address .= 'C.P. ' . $shipping_address['postcode'] . "\r\n";
 
 			$courier = 'Estafeta';
-			if(preg_match("|\bestafeta\b|i", $params['shipping_methods'])) {
+			if(preg_match("|\bestafeta\b|i", $order['shipping_methods'])) {
 				$courier = 'Estafeta';
-			} elseif(preg_match("|\bcorreos\b|i", $params['shipping_methods'])) {
+			} elseif(preg_match("|\bcorreos\b|i", $order['shipping_methods'])) {
 				$courier = 'Correos de México';
 			}
 
 			$data = array(
-		        'date' => $params['created_at'],
-		        'name' => $params['first_name'] . ' ' . $params['last_name'],
+		        'date' => $order['created_at'],
+		        'name' => $billing_address['first_name'] . ' ' . $billing_address['last_name'],
 		        'user' => null,
-		        'email' => $params['email'],
-		        'phone' => $params['phone'],
-		        'package' => implode(',', $package),
-		        'addressee' => $shipping_address['first_name'] . ' ' . $shipping_address['last_name'],
-		        'addressee_phone' => $shipping_address['phone'],
-		        'address' => $address,
-		        'courier' => $courier,
-		        'shipping_cost' => $params['total_shipping'],
-		        'total' => $params['subtotal'] - $params['total_discount'],
-		        'commission' => 0,
-		        'raw_material' => 0,
+		        'email' => $billing_address['email'],
+		        'phone' => $billing_address['phone'],
+		        'package' => $package,
 		        'split_earnings' => true,
 		        'from_inversions' => false,
 	    	);
+
+	    	$data['delivery']['addressee'] = $shipping_address['first_name'] . ' ' . $shipping_address['last_name'];
+		    $data['delivery']['phone'] = $billing_address['phone'];
+		    $data['delivery']['address'] = $address;
+		    $data['delivery']['courier'] = $courier;
+		    $data['delivery']['cost'] = $order['total_shipping'];
+		    $data['payment']['total'] = $order['subtotal'] - $order['total_discount'];
+		    $data['payment']['commission'] = 0;
+		    $data['payment']['rawMaterial'] = 0;
 
 	    	$this->load->model('sales_model');
 	    	$this->sales_model->create($data);
